ca6d12ab2c322c13003be6c217d3572ba18c0419
Alejandro Galue <agalue@opennms.org>
2021-12-02 08:55:23 -0500

    Simplify logic to start the dependencies.

--- start-dependencies.sh
+++ start-dependencies.sh
@@ -4,25 +4,16 @@
 # WARNING: For testing purposes only
 
 NAMESPACE="shared"
-PG_YAML="dependencies/postgresql.yaml"
-KAFKA_YAML="dependencies/kafka.yaml"
-TARGET_DIR="k8s/pki"
+TARGET_DIR="k8s/pki" # Expected location for the configMap that keeps the Truststore
 ONMS_USER_PASSWORD="0p3nNM5" # Must match KAFKA_SASL_PASSWORD from app-credentials
 TRUSTSTORE_FILE="kafka-truststore.jks" # Must be consistent with KAFKA_SSL_TRUSTSTORE_LOCATION from app-settings
 TRUSTSTORE_PASSWORD="0p3nNM5" # Must match KAFKA_SSL_TRUSTSTORE_PASSWORD from app-credentials
-CLUSTER_NAME="onms" # Must match the name of the cluster inside KAFKA_YAML
-
-for YAML in $KAFKA_YAML $PG_YAML; do
-  if [[ ! -e $YAML ]]; then
-    echo "Fatal: $YAML required"
-    exit 1
-  fi
-done
+CLUSTER_NAME="onms" # Must match the name of the cluster inside dependencies/kafka.yaml
 
 kubectl create namespace $NAMESPACE
 kubectl create secret generic kafka-user-credentials --from-literal="opennms=$ONMS_USER_PASSWORD" -n $NAMESPACE
 kubectl apply -f "https://strimzi.io/install/latest?namespace=$NAMESPACE" -n $NAMESPACE
-kubectl apply -f $KAFKA_YAML -n $NAMESPACE
+kubectl apply -f dependencies -n $NAMESPACE
 kubectl wait kafka/$CLUSTER_NAME --for=condition=Ready --timeout=300s -n $NAMESPACE
 
 mkdir -p $TARGET_DIR
@@ -33,8 +24,6 @@ kubectl get secret $CLUSTER_NAME-cluster-ca-cert -n $NAMESPACE -o jsonpath='{.da
 TEMP_TRUSTSTORE="/tmp/ca.truststore.$(date +%s)"
 keytool -import -trustcacerts -alias root -file $CERT_FILE_PATH -keystore $TEMP_TRUSTSTORE -storepass "$TRUSTSTORE_PASSWORD" -noprompt
 mv -f $TEMP_TRUSTSTORE $TARGET_DIR/$TRUSTSTORE_FILE
-
-kubectl apply -f $PG_YAML -n $NAMESPACE
 
 kubectl get all -n $NAMESPACE
 
