69445fbbfc4ea0e56eb1c85b670cd8267e1def69
Alejandro Galue <agalue@opennms.org>
2021-12-06 12:51:59 -0500

    Using JVM level Truststore.

--- start-minion.sh
+++ start-minion.sh
@@ -23,8 +23,8 @@ while [ $# -gt 0 ]; do
 done
 
 # JKS file
-jks_file="kafka-truststore.jks"
-jks_path="$(pwd)/jks/${jks_file}"
+jks_file="jks/truststore.jks"
+jks_path="$(pwd)/${jks_file}"
 if [ ! -e ${jks_path} ]; then
   echo "ERROR: ${jks_path} doesn't exist, aborting."
   exit 1
@@ -50,18 +50,17 @@ for module in rpc sink twin; do
       security.protocol: SASL_SSL
       sasl.mechanism: SCRAM-SHA-512
       sasl.jaas.config: org.apache.kafka.common.security.scram.ScramLoginModule required username="${kafka_user}" password="${kafka_passwd}";
-      ssl.truststore.location: /opt/minion/etc/${jks_file}
-      ssl.truststore.password: ${jks_passwd}
 EOF
 done
 
 # Start Minion via Docker
 docker run --name ${minion_id} -it --rm \
  -e TZ=America/New_York \
+ -e JAVA_OPTS="-XX:+AlwaysPreTouch -XX:+UseG1GC -XX:+UseStringDeduplication -Djavax.net.ssl.trustStore=/etc/java/${jks_file} -Djavax.net.ssl.trustStorePassword=${jks_passwd}" \
  -p ${karaf_port}:8201 \
  -p ${syslog_port}:1514/udp \
  -p ${snmp_port}:1162/udp \
- -v ${jks_path}:/opt/minion/etc/${jks_file} \
+ -v ${jks_path}:/etc/java/${jks_file} \
  -v $yaml:/opt/minion/minion-config.yaml \
  opennms/minion:${minion_version} -c
 rm -f $yaml
